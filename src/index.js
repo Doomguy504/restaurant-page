f9ec2dd38ac12b9b9898a330e8dac32eb543e5b8
Doom <dustinhm607@gmail.com>
2025-09-04 11:19:32 -0700

    add page generation from nav button clicks

--- src/index.js
+++ src/index.js
@@ -2,3 +2,33 @@ import './styles.css';
 import {generateHome} from './home.js';
 
 generateHome();
+
+const handleNavClicked = function(ev){
+    //clear all contents of main content div
+    const contentDiv = document.querySelector('#content');
+    while (contentDiv.firstChild){
+        contentDiv.removeChild(contentDiv.firstChild);
+    }
+
+    //generate content based on text inside button clicked
+    const buttonText = ev.currentTarget.textContent;
+    switch (buttonText) {
+        case 'Home':
+            generateHome();
+            break;
+        case 'Menu':
+            //TODO add menu generation function
+            break;
+        case 'About':
+            //TODO add about generation function
+            break;
+        default:
+            console.error(`Error: Unhandled switch case for button with text content "${buttonText}"`);
+            break;
+    }
+}
+
+const navButtons = document.querySelectorAll('nav button');
+navButtons.forEach((button) => {
+    button.addEventListener('click', handleNavClicked);
+});
